2cf4f637b959cd8908f2ebdf6f5628f158841971
Mucka <hugodelacour.pro@gmail.com>
2024-07-09 15:39:56 +0200

    chore : inital commit

---
+++ app/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { Bar, BarChart, CartesianGrid } from "recharts"
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+ 
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+ 
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button variant="outline">Button</Button>
+      <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ChartContainer>
+    </main>
+  );
+}
